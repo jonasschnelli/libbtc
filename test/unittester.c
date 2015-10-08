213289357ba49d03d9f278ca608ab0642b87bbd8
Jonas Schnelli <dev@jonasschnelli.ch>
2015-10-08 11:49:45 +0200

    add test facility

---
+++ test/unittester.c
@@ -0,0 +1,34 @@
+/**********************************************************************
+ * Copyright (c) 2015 Jonas Schnelli                                  *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#if defined HAVE_CONFIG_H
+#include "libbtc-config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef HAVE_BUILTIN_EXPECT
+#define EXPECT(x,c) __builtin_expect((x),(c))
+#else
+#define EXPECT(x,c) (x)
+#endif
+
+#define TEST_FAILURE(msg) do { \
+    fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, msg); \
+    abort(); \
+} while(0)
+
+#define CHECK(cond) do { \
+    if (EXPECT(!(cond), 0)) { \
+        TEST_FAILURE("test condition failed: " #cond); \
+    } \
+} while(0)
+
+int main(int argc, char **argv)
+{
+	
+}
